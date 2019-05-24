0796ed5ee85cac1e61953ef4fc70c3b62c4ccdaa
Thomas Vincent-Cross <me@tvc.id.au>
2019-05-24 15:42:17 +1000

    Refactor Main

--- Program.cs
+++ Program.cs
@@ -5,49 +5,54 @@ using System.Text;
 
 namespace FEProxy {
 	class Program {
-		static void Main( string[] args ) {
-			ushort loginPort = UInt16.Parse( args[0] );
-			ushort worldPort = UInt16.Parse( args[1] );
-			string remoteHost = args[2];
-			ushort remotePort = UInt16.Parse( args[3] );
+		static Proxy loginProxy, worldProxy;
+		static ProxyListener loginProxyListener, worldProxyListener;
 
-			Proxy loginProxy, worldProxy;
-			ProxyListener worldProxyListener;
+		static string remoteHost;
+		static ushort loginPort, worldPort, remotePort;
 
-			ProxyListener loginProxyListener = new ProxyListener( loginPort, remoteHost, remotePort );
-			loginProxyListener.ProxyAccepted += ( sender, proxyEventArgs ) => {
-				loginProxy = proxyEventArgs.Proxy;
-				loginProxy.ServerPacketReceived += ( sender_, packetEventArgs ) => {
-					if( packetEventArgs.PacketType == 0x49 ) {
-						int offset = 4;
-						while( packetEventArgs.PacketData[offset++] != 0 );
+		static void LoginServerPacketReceived( object _, PacketEventArgs packetEventArgs ) {
+			if( packetEventArgs.PacketType == 0x49 ) {
+				int offset = 4;
+				while( packetEventArgs.PacketData[offset++] != 0 );
 
-						string loopBack = IPAddress.Loopback.ToString();
-						string worldRemoteHost = Encoding.ASCII.GetString( packetEventArgs.PacketData, 4, offset - 5 );
-						ushort worldRemotePort = (ushort)BitConverter.ToUInt32( packetEventArgs.PacketData, offset );
-						offset += 4;
+				string loopBack = IPAddress.Loopback.ToString();
+				string worldRemoteHost = Encoding.ASCII.GetString( packetEventArgs.PacketData, 4, offset - 5 );
+				ushort worldRemotePort = (ushort)BitConverter.ToUInt32( packetEventArgs.PacketData, offset );
+				offset += 4;
 
-						worldProxyListener = new ProxyListener( worldPort, worldRemoteHost, worldRemotePort );
-						worldProxyListener.ProxyAccepted += ( sender__, proxyEventArgs_ ) => {
-							worldProxy = proxyEventArgs_.Proxy;
-							worldProxy.Process();
-						};
-						worldProxyListener.Process( true );
-
-						using( MemoryStream stream = new MemoryStream() ) {
-							stream.Write( packetEventArgs.PacketData, 0, 4 );
-							stream.Write( Encoding.ASCII.GetBytes( loopBack ), 0, Encoding.ASCII.GetByteCount( loopBack ) );
-							stream.WriteByte( 0 );
-							stream.Write( BitConverter.GetBytes( (uint)worldPort ), 0, 4 );
-							stream.Write( packetEventArgs.PacketData, offset, packetEventArgs.PacketLength - offset );
-
-							byte[] buffer = new byte[stream.Length];
-							Array.Copy( stream.GetBuffer(), buffer, buffer.Length );
-
-							packetEventArgs.SetPacketData( buffer );
-						}
-					}
+				worldProxyListener = new ProxyListener( worldPort, worldRemoteHost, worldRemotePort );
+				worldProxyListener.ProxyAccepted += ( __, proxyEventArgs ) => {
+					worldProxy = proxyEventArgs.Proxy;
+					worldProxy.Process();
 				};
+				worldProxyListener.Process( true );
+
+				using( MemoryStream stream = new MemoryStream() ) {
+					stream.Write( packetEventArgs.PacketData, 0, 4 );
+					stream.Write( Encoding.ASCII.GetBytes( loopBack ), 0, Encoding.ASCII.GetByteCount( loopBack ) );
+					stream.WriteByte( 0 );
+					stream.Write( BitConverter.GetBytes( (uint)worldPort ), 0, 4 );
+					stream.Write( packetEventArgs.PacketData, offset, packetEventArgs.PacketLength - offset );
+
+					byte[] buffer = new byte[stream.Length];
+					Array.Copy( stream.GetBuffer(), buffer, buffer.Length );
+
+					packetEventArgs.SetPacketData( buffer );
+				}
+			}
+		}
+
+		static void Main( string[] args ) {
+			loginPort = UInt16.Parse( args[0] );
+			worldPort = UInt16.Parse( args[1] );
+			remoteHost = args[2];
+			remotePort = UInt16.Parse( args[3] );
+
+			loginProxyListener = new ProxyListener( loginPort, remoteHost, remotePort );
+			loginProxyListener.ProxyAccepted += ( _, proxyEventArgs ) => {
+				loginProxy = proxyEventArgs.Proxy;
+				loginProxy.ServerPacketReceived += LoginServerPacketReceived;
 				loginProxy.Process();
 			};
 			loginProxyListener.Process( true );
